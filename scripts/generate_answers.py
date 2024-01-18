efc79c9c95ecc32d92e874ca3cd0d32d72fc4ed7
Olly Styles <ollystyles@gmail.com>
2024-01-18 12:11:42 +0000

    support for multi-step questions

--- scripts/generate_answers.py
+++ scripts/generate_answers.py
@@ -7,14 +7,15 @@ from src.tools import calendar
 
 OPENAI_KEY = open("openai_key.txt", "r").read()
 questions = pd.read_csv(
-    "data/processed/questions_and_answers.csv", usecols=["question"]
+    "data/processed/questions_and_answers_multi_step.csv", usecols=["question"]
 )["question"].tolist()
 
-results = pd.DataFrame(columns=["question", "answer"])
+results = pd.DataFrame(columns=["question", "answer", "function_calls"])
 # llm = ChatOpenAI(model_name="gpt-4", openai_api_key=OPENAI_KEY, temperature=0)
 # llm = OpenAI(
 #     model_name="gpt-3.5-turbo-instruct", openai_api_key=OPENAI_KEY, temperature=0
 # )
+Note the the latest version of GPT3.5 (below) does not show it's chain of thoughts for some reason
 llm = ChatOpenAI(
     model_name="gpt-3.5-turbo-1106", openai_api_key=OPENAI_KEY, temperature=0
 )
@@ -34,17 +35,18 @@ agent = initialize_agent(
 
 for question in questions:
     response = agent({"input": question})
-    last_function_call = convert_agent_action_to_function_call(
-        response["intermediate_steps"][0][-2]
-    )
+    function_calls = []
+    for step in response["intermediate_steps"]:
+        function_calls.append(convert_agent_action_to_function_call(step[-2]))
     print(f"### Question: {question}")
-    print(f"### Answer: {last_function_call}")
+    print(f"### Answer: {function_calls[-1]}")
+    # convert function_calls to string
     results = pd.concat(
         [
             results,
             pd.DataFrame(
-                [[question, last_function_call]],
-                columns=["question", "answer"],
+                [[question, function_calls[-1], ",".join(function_calls[:-1])]],
+                columns=["question", "answer", "function_calls"],
             ),
         ],
         ignore_index=True,
