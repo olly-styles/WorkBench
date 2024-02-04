ae2e87bd47b390d67f25ebbe856665f5076cab67
Sam Miller <sam@atlasai.co.uk>
2024-02-04 11:10:58 +0000

    remove redundant shell script

--- eval_all.sh
+++
@@ -1,2 +0,0 @@
-echo multi_domain_questions_and_answers GPT-4
-python scripts/evals/calculate_metrics.py --ground_truth_path "data/processed/multi_domain_questions_and_answers.csv" --predictions_path "data/results/answers_gpt-4-0125-preview2024-01-31 16:01:08.987939.csv"
