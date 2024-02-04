95b249165b34d41c2772589a28360d0b0fa97219
Sam Miller <sam@atlasai.co.uk>
2024-02-04 11:20:25 +0000

    clean up gen results script

--- src/evals/utils.py
+++ src/evals/utils.py
@@ -2,7 +2,14 @@ from src.tools.toolkits import tool_information
 import re
 import os
 import pandas as pd
+from langchain_openai import ChatOpenAI, OpenAI
+from langchain.agents import initialize_agent, AgentType
+import csv
+from src.tools import calendar, email
+from src.tools.toolkits import calendar_toolkit, email_toolkit
 
+
+OPENAI_KEY = open("openai_key.txt", "r").read()
 
 def convert_agent_action_to_function_call(action):
     """Converts langchain_core.agents.AgentAction to an API call"""
@@ -95,3 +102,90 @@ def get_latest_results_from_dir(results_root_dir, tool, action, model_list):
             ground_truth = pd.read_csv(ground_truth_path, dtype=str)
             print(f"\nCalculating metrics for {tool}, {action} action with {model}")
             calculate_metrics(ground_truth, predictions, print_errors=False)
+
+
+def generate_results(questions_path, model_name):
+    """Generates results for a given model and set of questions. Saves the results to a csv file."""
+    questions = pd.read_csv(
+        questions_path,
+        usecols=["question"],
+    )["question"].tolist()
+
+    results = pd.DataFrame(
+        columns=["question", "function_calls", "full_response", "stopped"]
+    )
+
+    if model_name == "gpt-3.5-turbo-instruct":
+        llm = OpenAI(
+            model_name="gpt-3.5-turbo-instruct",
+            openai_api_key=OPENAI_KEY,
+            temperature=0,
+            model_kwargs={"seed": 42},
+        )
+    elif model_name == "gpt-4-0125-preview":
+        llm = ChatOpenAI(
+            model_name="gpt-4-0125-preview",
+            openai_api_key=OPENAI_KEY,
+            temperature=0,
+            model_kwargs={"seed": 42},
+        )
+    else:
+        raise ValueError(
+            "Invalid --model_name. Must be gpt-3.5-turbo-instruct or gpt-4-0125-preview."
+        )
+
+
+    agent = initialize_agent(
+        llm=llm,
+        agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        tools=email_toolkit + calendar_toolkit,
+        verbose=True,
+        return_intermediate_steps=True,
+        max_iterations=5,
+    )
+    agent.agent.llm_chain.prompt.messages[0].prompt.template = (
+        "The year is 2023. " + agent.agent.llm_chain.prompt.messages[0].prompt.template
+    )
+
+    for question in questions:
+        response = agent({"input": question})
+        function_calls = []
+        for step in response["intermediate_steps"]:
+            function_calls.append(convert_agent_action_to_function_call(step[-2]))
+
+        agent_stopped = (
+            True
+            if response["output"] == "Agent stopped due to iteration limit or time limit."
+            else False
+        )
+
+        print(f"### Question: {question}")
+        print(f"### Answer: {function_calls}")
+
+        results = pd.concat(
+            [
+                results,
+                pd.DataFrame(
+                    [[question, ",".join(function_calls), str(response), agent_stopped]],
+                    columns=["question", "function_calls", "full_response", "stopped"],
+                ),
+            ],
+            ignore_index=True,
+        )
+        # Reset all data after each question
+        calendar.CALENDAR_EVENTS = pd.read_csv(
+            "data/processed/calendar_events.csv", dtype=str
+        )
+        email.EMAILS = pd.read_csv("data/processed/emails.csv", dtype=str)
+
+
+    question_type = questions_path.split("/")[-1].split(".")[0].replace("questions_and_answers_", "")
+    domain, action_length = question_type.split("_")[:2]
+    save_dir = os.path.join("data", "results", domain, action_length)
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Removes microseconds and makes it more readable
+    current_datetime = str(pd.Timestamp.now()).split(".")[0].replace(" ", "_").replace(":", "-")
+    save_path = os.path.join(save_dir, model_name + "_" + current_datetime + ".csv")
+    results.to_csv(save_path, index=False, quoting=csv.QUOTE_ALL)
+    return results
