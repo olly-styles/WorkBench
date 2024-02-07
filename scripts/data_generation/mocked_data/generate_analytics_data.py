9f46902775839a6ad07648d1f93167dd23ec1d20
Olly Styles <ollystyles@gmail.com>
2024-02-07 17:09:43 +0000

    add analytics tools

---
+++ scripts/data_generation/mocked_data/generate_analytics_data.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+from datetime import timedelta, datetime
+
+np.random.seed(42)
+
+
+def generate_visitor_id(used_ids):
+    visitor_id = str(np.random.randint(0, 999)).zfill(3)
+    while visitor_id in used_ids:
+        visitor_id = generate_visitor_id(used_ids)
+    return visitor_id
+
+
+def generate_page_views():
+    return int(np.random.exponential(5) + 1)
+
+
+def generate_session_duration_seconds():
+    return int(np.random.exponential(20))
+
+
+def generate_traffic_source():
+    return np.random.choice(
+        ["direct", "referral", "search engine", "social media"], p=[0.5, 0.1, 0.1, 0.3]
+    )
+
+
+def generate_visit_date(start_date, end_date):
+    time_between_dates = end_date - start_date
+    days_between_dates = time_between_dates.days
+    random_number_of_days = np.random.randint(0, days_between_dates)
+    return start_date + timedelta(days=random_number_of_days)
+
+
+# Parameters for data generation
+start_date = datetime(2023, 10, 1)
+end_date = datetime(2023, 12, 31)
+num_records = 500
+
+# Data structure to keep track of used visitor IDs per day
+used_visitor_ids_per_day = {}
+
+# Generate data
+records = []
+for _ in range(num_records):
+    date_of_visit = generate_visit_date(start_date, end_date)
+    used_ids = used_visitor_ids_per_day.get(date_of_visit, set())
+    visitor_id = generate_visitor_id(used_ids)
+    used_ids.add(visitor_id)
+    used_visitor_ids_per_day[date_of_visit] = used_ids
+
+    session_duration_seconds = generate_session_duration_seconds()
+    page_views = generate_page_views()
+    user_engaged = (session_duration_seconds > 30) & (page_views > 3)
+    record = {
+        "date_of_visit": date_of_visit,
+        "visitor_id": visitor_id,
+        "page_views": page_views,
+        "session_duration_seconds": session_duration_seconds,
+        "traffic_source": generate_traffic_source(),
+        "user_engaged": user_engaged,
+    }
+    records.append(record)
+
+analytics_data = pd.DataFrame(records)
+analytics_data.to_csv("data/processed/analytics_data.csv", index=False)
---
+++ src/tools/analytics.py
@@ -0,0 +1,179 @@
+import pandas as pd
+from langchain.tools import tool
+
+ANALYTICS_DATA = pd.read_csv("data/processed/analytics_data.csv", dtype=str)
+PLOTS_DATA = pd.DataFrame(columns=["file_path"])
+
+
+@tool("analytics.get_visitor_information_by_id", return_direct=False)
+def get_visitor_information_by_id(visitor_id=None):
+    """
+    Returns the analytics data for a given visitor ID.
+
+    Parameters
+    ----------
+    visitor_id : str, optional
+        ID of the visitor.
+
+    Returns
+    -------
+    visitor_data : dict
+        Analytics data for the given visitor ID.
+
+    Examples
+    --------
+    >>> analytics.get_visitor_information_by_id("000")
+    {{"date_of_visit": "2023-10-01", "visitor_id": "000", "page_views": "3", "session_duration_seconds": "10.0", "traffic_source": "search engine", "user_engaged": "False"}}
+
+    """
+    if not visitor_id:
+        return "Visitor ID not provided."
+    visitor_data = ANALYTICS_DATA[ANALYTICS_DATA["visitor_id"] == visitor_id].to_dict(
+        orient="records"
+    )
+    if visitor_data:
+        return visitor_data
+    else:
+        return "Visitor not found."
+
+
+@tool("analytics.create_plot", return_direct=False)
+def create_plot(time_min=None, time_max=None, value_to_plot=None):
+    """
+    Plots the analytics data for a given time range and value.
+
+    Parameters
+    ----------
+    time_min : str, optional
+        Start date of the time range. Date format is "YYYY-MM-DD".
+    time_max : str, optional
+        End date of the time range. Date format is "YYYY-MM-DD".
+    value_to_plot : str, optional
+        Value to plot. Available values are: "page_views", "session_duration_seconds", "traffic_source", "user_engaged"
+
+    Returns
+    -------
+    file_path : str
+        Path to the plot file. Filename is {time_min}_{time_max}_{value_to_plot}.png.
+
+    Examples
+    --------
+    >>> analytics.create_plot("2023-10-01", "2023-12-31", "page_views")
+    "plots/2023-10-01_2023-12-31_page_views.png"
+
+    """
+    global PLOTS_DATA
+    if not time_min:
+        return "Start date not provided."
+    if not time_max:
+        return "End date not provided."
+    if not value_to_plot:
+        return "Value to plot not provided."
+    # Plot the data here and save it to a file
+    file_path = f"plots/{time_min}_{time_max}_{value_to_plot}.png"
+    PLOTS_DATA.loc[len(PLOTS_DATA)] = [file_path]
+    return file_path
+
+
+@tool("analytics.total_visits_count", return_direct=False)
+def total_visits_count(time_min=None, time_max=None):
+    """
+    Returns the total number of visits within a specified time range.
+
+    Parameters
+    ----------
+    time_min : str, optional
+        Start date of the time range. Date format is "YYYY-MM-DD".
+    time_max : str, optional
+        End date of the time range. Date format is "YYYY-MM-DD".
+
+    Returns
+    -------
+    total_visits : int
+        Total number of visits in the specified time range.
+
+    Examples
+    --------
+    >>> analytics.total_visits_count("2023-10-01", "2023-12-31")
+    500
+    """
+    if time_min:
+        data = ANALYTICS_DATA[ANALYTICS_DATA["date_of_visit"] >= time_min]
+    else:
+        data = ANALYTICS_DATA
+    if time_max:
+        data = data[data["date_of_visit"] <= time_max]
+
+    total_visits = data.shape[0]
+    return total_visits
+
+
+@tool("analytics.engaged_users_count", return_direct=False)
+def engaged_users_count(time_min=None, time_max=None):
+    """
+    Returns the number of engaged users within a specified time range.
+
+    Parameters
+    ----------
+    time_min : str, optional
+        Start date of the time range. Date format is "YYYY-MM-DD".
+    time_max : str, optional
+        End date of the time range. Date format is "YYYY-MM-DD".
+
+    Returns
+    -------
+    engaged_users : int
+        Number of engaged users in the specified time range.
+
+    Examples
+    --------
+    >>> analytics.engaged_users_count("2023-10-01", "2023-12-31")
+    100
+    """
+    if time_min:
+        data = ANALYTICS_DATA[ANALYTICS_DATA["date_of_visit"] >= time_min]
+    else:
+        data = ANALYTICS_DATA
+    if time_max:
+        data = data[data["date_of_visit"] <= time_max]
+
+    engaged_users = data[data["user_engaged"]].shape[0]
+    return engaged_users
+
+
+@tool("analytics.traffic_source_count", return_direct=False)
+def traffic_source_count(time_min=None, time_max=None, traffic_source=None):
+    """
+    Returns the number of visits from a specific traffic source within a specified time range.
+
+    Parameters
+    ----------
+    time_min : str, optional
+        Start date of the time range. Date format is "YYYY-MM-DD".
+    time_max : str, optional
+        End date of the time range. Date format is "YYYY-MM-DD".
+    traffic_source : str, optional
+        Traffic source to filter the visits.
+
+    Returns
+    -------
+    traffic_source_visits : int
+        Number of visits from the specified traffic source in the specified time range.
+
+    Examples
+    --------
+    >>> analytics.traffic_source_count("2023-10-01", "2023-12-31", "search engine")
+    250
+    """
+    if time_min:
+        data = ANALYTICS_DATA[ANALYTICS_DATA["date_of_visit"] >= time_min]
+    else:
+        data = ANALYTICS_DATA
+    if time_max:
+        data = data[data["date_of_visit"] <= time_max]
+
+    if traffic_source:
+        traffic_source_visits = data[data["traffic_source"] == traffic_source].shape[0]
+    else:
+        traffic_source_visits = data.shape[0]
+    return traffic_source_visits
