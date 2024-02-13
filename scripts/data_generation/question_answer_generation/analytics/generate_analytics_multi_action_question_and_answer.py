ff953c9c4531419ccb4a4ab12b2fc624a6602648
Sam Miller <sam@atlasai.co.uk>
2024-02-13 14:15:21 +0000

    analytics refactor

--- scripts/data_generation/question_answer_generation/analytics/generate_analytics_multi_action_question_and_answer.py
+++ scripts/data_generation/question_answer_generation/analytics/generate_analytics_multi_action_question_and_answer.py
@@ -6,41 +6,32 @@ import os
 
 project_root = os.path.abspath(os.path.curdir)
 sys.path.append(project_root)
-
-
 random.seed(42)
+
+from src.evals.utils import generate_all_questions_and_answers
+from src.data_generation.data_generation_utils import get_natural_language_date
+
+def page_views_plot_logic():
+    time_min = "2023-10-01"
+    time_max = "2023-10-31"
+    date_min = get_natural_language_date(time_min)
+    date_max = get_natural_language_date(time_max)
+    return {"time_min": time_min, "time_max": time_max, "date_min": date_min, "date_max": date_max}
 
 MULTI_ACTION_TEMPLATES = [
     {
-        "question": "Make a plot of page views from {time_min} to {time_max}",
+        "question": "Make a plot of page views from {date_min} to {date_max}",
         "answer": [
             """analytics.create_plot.func(time_min='{time_min}', time_max='{time_max}', value_to_plot='page_views', plot_type='line')"""
         ],
+        "logic": page_views_plot_logic
     },
 ]
 
-# Generate a limited number of unique multi-action questions and answers
 max_questions_per_template = 1  # Limit the number of questions per template
 
 if __name__ == "__main__":
-    generated_questions_and_answers = []
-    for template in MULTI_ACTION_TEMPLATES:
-        for _ in range(max_questions_per_template):
-            time_min = "2023-10-01"
-            time_max = "2023-10-31"
-            question = template["question"].format(time_min=time_min, time_max=time_max)
-            answer = []
-            for step in template["answer"]:
-                answer.append(step.format(time_min=time_min, time_max=time_max))
-            questions = [q["question"] for q in generated_questions_and_answers]
-            if question not in questions:
-                generated_questions_and_answers.append(
-                    {"question": question, "answer": answer, "template": template}
-                )
-            for question_and_answer in generated_questions_and_answers:
-                print(question_and_answer["question"])
-                print(question_and_answer["answer"])
-                print(question_and_answer["template"])
+    generated_questions_and_answers = generate_all_questions_and_answers(MULTI_ACTION_TEMPLATES, max_questions_per_template)
     df = pd.DataFrame(generated_questions_and_answers)
     df.to_csv(
         "data/processed/analytics_questions_and_answers_multi_action.csv",
