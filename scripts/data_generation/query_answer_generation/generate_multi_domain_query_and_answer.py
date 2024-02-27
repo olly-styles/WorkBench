9d430286e7f7fd4d99cc309d4c77efa1cd113d47
Olly Styles <ollystyles@gmail.com>
2024-02-27 15:48:34 +0000

    two more multi domain logic

--- scripts/data_generation/query_answer_generation/generate_multi_domain_query_and_answer.py
+++ scripts/data_generation/query_answer_generation/generate_multi_domain_query_and_answer.py
@@ -8,6 +8,7 @@ project_root = os.path.abspath(os.path.curdir)
 sys.path.append(project_root)
 
 from src.data_generation.data_generation_utils import (
+    HARDCODED_CURRENT_TIME,
     format_event_duration,
     generate_event_duration_minutes,
     get_natural_language_date,
@@ -31,6 +32,7 @@ random.seed(42)
 emails_data = pd.read_csv("data/processed/emails.csv", dtype=str)
 calendar_events = pd.read_csv("data/processed/calendar_events.csv", dtype=str)
 dates = list(calendar_events["event_start"].str.split(" ").str[0].unique())
+times = list(calendar_events["event_start"].str.split(" ").str[1].unique())
 
 
 def find_email_schedule_meeting_sender_logic():
@@ -87,7 +89,7 @@ def get_metric_fell_create_task_logic():
     if fell_vs_grew["fell_vs_grew"] == "fell":
         answer = [
             get_new_task_string(
-                new_task_dict["task_name"], new_task_dict["name"], new_task_dict["board"], new_task_dict["due_date"]
+                new_task_dict["task_name"], new_task_dict["email"], new_task_dict["board"], new_task_dict["due_date"]
             )
         ]
     else:
@@ -119,6 +121,66 @@ def get_metric_fell_create_task_book_meeting_send_email_logic():
     return {**create_task_book_meeting_dict, "answer": answer}
 
 
+def schedule_meeting_if_no_emails_logic():
+    email = random.choice(emails_data["sender/recipient"])
+    name = email.split(".")[0]
+    days_since_email = random.randint(2, 4)
+    dates_up_to_6_days_in_the_future = [str(HARDCODED_CURRENT_TIME.date() + pd.Timedelta(days=i)) for i in range(1, 7)]
+    meeting_date = random.choice(dates_up_to_6_days_in_the_future)
+
+    # while this date is a weekend, pick another date
+    while pd.to_datetime(meeting_date).dayofweek >= 5:
+        meeting_date = random.choice(dates_up_to_6_days_in_the_future)
+    time = random.choice(times)
+    natural_language_time = get_natural_language_time(time)
+
+    answer = []
+    if emails_data[
+        (emails_data["sender/recipient"] == email)
+        & (emails_data["sent_datetime"] > str(HARDCODED_CURRENT_TIME - pd.Timedelta(days=days_since_email)))
+    ].empty:
+        answer.append(
+            f"calendar.create_event.func(event_name='Catch up with {name}', participant_email='{email}', event_start='{meeting_date} {time}', duration='30')"
+        )
+
+    return {
+        "name": name,
+        "days": days_since_email,
+        "day_of_week": pd.to_datetime(meeting_date).day_name(),
+        "natural_language_time": natural_language_time,
+        "answer": answer,
+    }
+
+
+def send_email_if_no_meetings_logic():
+    """If I haven't met with {name} in the past {days} days, send them an email titled 'Catch up soon?' saying 'We haven't caught up since {last_meeting_date} - can you send some availability over next week?'"""
+    email = random.choice(calendar_events["participant_email"].unique())
+    name = email.split(".")[0]
+    threshold_days_since_last_meeting = random.randint(2, 4)
+    last_meeting_date = (
+        calendar_events[
+            (calendar_events["participant_email"] == email)
+            & (calendar_events["event_start"] < str(HARDCODED_CURRENT_TIME))
+        ]
+        .sort_values("event_start", ascending=False)
+        .iloc[0]["event_start"]
+        .split(" ")[0]
+    )
+    threshold_date = str((HARDCODED_CURRENT_TIME - pd.Timedelta(days=threshold_days_since_last_meeting)).date())
+
+    answer = []
+    if last_meeting_date < threshold_date:
+        subject = "Catch up soon?"
+        body = f"We have not caught up in over {threshold_days_since_last_meeting} days - can you send some availability over next week?"
+        answer.append(f"email.send_email.func(recipient='{email}', subject='{subject}', body='{body}')")
+    return {
+        "name": name,
+        "days": threshold_days_since_last_meeting,
+        "last_meeting_date": last_meeting_date,
+        "answer": answer,
+    }
+
+
 MULTI_DOMAIN_TEMPLATES = [
     {
         "query": """Find the email from {natural_language_email_date} about '{subject}' and schedule a {natural_language_duration} meeting called '{subject}' at {natural_language_time} with the sender for {natural_language_meeting_date}.""",
@@ -129,10 +191,12 @@ MULTI_DOMAIN_TEMPLATES = [
         "logic": find_event_send_email_logic,
     },
     {
-        "query": """If {name} hasn't sent me any emails in the past {days} days, schedule a {duration} meeting with them for {day_of_week} at {natural_language_time}.""",
+        "query": """If {name} hasn't sent me any emails in the past {days} days, schedule a 30 minute meeting with them for {day_of_week} at {natural_language_time} called 'Catch up with {name}'""",
+        "logic": schedule_meeting_if_no_emails_logic,
     },
     {
-        "query": """If I haven't met with {name} in the past {days} days, send them an email titled 'Catch up soon?' saying 'We haven't caught up since {last_meeting_date} - can you send some availability over next week?'""",
+        "query": """If I haven't met with {name} in the past {days} days, send them an email titled 'Catch up soon?' saying 'We have not caught up in over {days} days - can you send some availability over next week?'""",
+        "logic": send_email_if_no_meetings_logic,
     },
     {
         "query": """If I don't have any meetings scheduled with {name} in the next {days} days, send them an email titled 'Catch up soon?' saying 'We haven't caught up since {last_meeting_date} - can you send some availability over next week?'""",
@@ -175,23 +239,16 @@ MULTI_DOMAIN_TEMPLATES = [
         and title it  'Discuss {natural_language_metric}'""",
     },
     {
-        "query": """If {natural_language_metric} fell since {date_min}, make a task for {name} called 'Improve {natural_language_metric}', which is due in a week.""",
-        "logic": get_metric_fell_create_task_logic,
-    },
-    {
         "query": """If {natural_language_metric} was {more_or_less} than {threshold} at any time since {date_min}, 
         email {name} saying 'I noticed {natural_language_metric} was {more_or_less} than {threshold} recently - can we discuss?' 
         and title it 'Discuss {natural_language_metric}'""",
     },
-    {  # examples of 3-domain query
+    # 3 domain
+    {
         "query": """If {netural_language_metric} {fell_or_grew} since {date_min},
         email {name} saying 'We'r e not doing well on {natural_language_metric} recently - can we discuss?'
         and title it 'Discuss {natural_language_metric}'. 
         then also book a meeting with them called 'Catch up on {natural_language_metric}' at the earliest time I'm free tomorrow""",
-    },
-    {
-        "query": """If {natural_language_metric} fell since {natural_language_date}, make a {board} backlog task for {name} called 'Improve {natural_language_metric}', which is due in a week, and then book a meeting with them called 'Catch up on {natural_language_metric}' for the earliest time I'm free tomorrow.""",
-        "logic": get_metric_fell_create_task_book_meeting_logic,
     },
     {
         "query": """If {name} has any overdue tasks, book a meeting with them called 'Catch up on overdue tasks' at the earliest time I'm free tomorrow.
@@ -205,27 +262,9 @@ MULTI_DOMAIN_TEMPLATES = [
         Otherwise, email {name} saying 'I just checked {natural_language_metric} since {date_min} and they're doing great - nice work!' 
         Title it 'Nice work on {natural_language_metric}'""",
     },
-    # examples of 4-domain query
-    {
-        "query": """If {natural_language_metric} fell since {natural_language_date}, \
-make a task for {name} called 'Improve {natural_language_metric}', which is due in a week. \
-Also send them an email saying 'I need you to look at {natural_language_metric} - more details on the task I just made.' \
-and title it 'Discuss {natural_language_metric}'. \
-then also book a 30-minute meeting with them called 'Catch up on {natural_language_metric}' at the earliest time I'm free tomorrow""",
-        "logic": get_metric_fell_create_task_book_meeting_send_email_logic,
-    },
-    {
-        "query": """If {natural_language_metric} was {more_or_less} than {threshold} on {natural_language_date},
-        take the person with the fewest backlog tasks on {board} and book a meeting with them
-        called 'Discuss {natural_language_metric}' at the earliest time I'm free tomorrow.
-        Then also make a task for them called 'Improve {natural_language_metric}' on {board}, which is due in a week.
-        Otherwise, email {name} saying 'I just checked {natural_language_metric} and you're doing doing great - nice work!' 
-        Title it 'Nice work on {natural_language_metric}'
-        """,
-    },
 ]
 
-max_queries_per_template = 1
+max_queries_per_template = 3
 if __name__ == "__main__":
     MULTI_DOMAIN_TEMPLATES = [t for t in MULTI_DOMAIN_TEMPLATES if "logic" in t.keys()]
     generated_queries_and_answers = generate_all_queries_and_answers(MULTI_DOMAIN_TEMPLATES, max_queries_per_template)
