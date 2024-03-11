b7f4c9fe47cc011cdd2e69b0e25b4a26a351fe92
Sam Miller <sam@atlasai.co.uk>
2024-03-11 04:17:27 +0000

    Harder queries now running

--- scripts/data_generation/query_answer_generation/generate_analytics_query_and_answer.py
+++ scripts/data_generation/query_answer_generation/generate_analytics_query_and_answer.py
@@ -17,7 +17,7 @@ from src.tools.analytics import METRICS, METRIC_NAMES, ANALYTICS_DATA
 dates = ANALYTICS_DATA["date_of_visit"].unique()
 metric_naming_dict = {metric: name for metric, name in zip(METRICS, METRIC_NAMES)}
 metric_to_func_dict = {
-    "page_views": analytics.total_visits_count,
+    "total_visits": analytics.total_visits_count,
     "session_duration_seconds": analytics.get_average_session_duration,
     "traffic_source": analytics.traffic_source_count,
     "user_engaged": analytics.engaged_users_count,
@@ -32,29 +32,32 @@ def get_random_dict():
     date_min = random.choice(dates)
     metric = random.choice(METRICS)
     metric2 = random.choice([m for m in METRICS if m != metric])
-    more_or_less = random.choice(["more", "less"])
-    fell_or_grew = random.choice(["fell", "grew"])
     threshold = get_threshold(metric)
+    growth_threshold = random.choice([0.1, 0.2, 0.3, 0.4, 0.5])
+    natural_language_growth_threshold = f"{int(100*growth_threshold)}%"
     return {
         "date_min": date_min,
-        "date_max": str(HARDCODED_CURRENT_TIME.date()),
+        "date_max": str(ANALYTICS_DATA["date_of_visit"].max()),
         "metric": metric,
         "metric2": metric2,
         "natural_language_date": get_natural_language_date(date_min),
         "natural_language_metric": metric_naming_dict[metric],
         "natural_language_metric_2": metric_naming_dict[metric2],
         "plot_type": random.choice(["line", "bar", "scatter", "histogram"]),
-        "more_or_less": more_or_less,
+        "more_or_less": random.choice(["more", "less"]),
+        "fallen_or_grown": random.choice(["fallen", "grown"]),
         "threshold": threshold,
-        "fell_or_grew": fell_or_grew,
+        "growth_threshold": growth_threshold,
+        "natural_language_growth_threshold": natural_language_growth_threshold,
     }
 
 
 def get_threshold(metric):
     """Gets the threshold for a given metric over the whole time series"""
-    data = ANALYTICS_DATA[["date_of_visit", metric]]
-    data.loc[:, metric] = data[metric].astype(float)
-    threshold_percentage = random.randint(1, 99)
+    metric_name = "page_views" if metric == "total_visits" else metric
+    data = ANALYTICS_DATA[["date_of_visit", metric_name]]
+    data.loc[:, metric_name] = data[metric_name].astype(float)
+    threshold_percentage = random.choice([0, 50, 100])
     threshold = np.percentile(data.groupby("date_of_visit").mean(), threshold_percentage)
     return int(threshold)
 
@@ -64,48 +67,12 @@ def metric_more_or_less(metric, date_min, threshold):
     metric_on_date = metric_series[date_min]
     return "more" if metric_on_date > threshold else "less"
 
-
-def metric_more_or_less_any_time(metric, date_min, threshold):
-    series_func = metric_to_func_dict[metric]
-    metric_series = pd.Series(series_func(date_min))
-    if (metric_series > threshold).sum() == 0:
-        return "less"
-    elif (metric_series < threshold).sum() == 0:
-        return "more"
-    else:
-        return "both more and less"
-
-def get_threshold_and_metric_more_or_less():
-    base_dict = get_random_dict()
-    metric_vs_threshold = metric_more_or_less(base_dict["metric"], base_dict["date_min"], base_dict["threshold"])
-    return {**base_dict, "metric_vs_threshold": metric_vs_threshold}
-
-
-def fell_or_grew(metric, start_date, end_date):
-    """Returns True if the metric grew or fell between the start and end date"""
-    metric_series = metric_to_func_dict[metric](start_date, end_date)
-    return "grew" if metric_series[end_date] > metric_series[start_date] else "fell"
-
-
-def get_metric_fell_vs_grew():
-    base_dict = get_random_dict()
-    base_dict["date_max"] = str(
-        (pd.to_datetime(base_dict["date_min"]) + pd.Timedelta(days=random.randint(7, 30))).date()
-    )
-    if base_dict["date_max"] > str(HARDCODED_CURRENT_TIME.date()):
-        base_dict["date_max"] = str(HARDCODED_CURRENT_TIME.date())
-    base_dict["natural_language_date_max"] = get_natural_language_date(base_dict["date_max"])
-    fell_vs_grew = fell_or_grew(base_dict["metric"], base_dict["date_min"], base_dict["date_max"])
-    return {**base_dict, "fell_vs_grew": fell_vs_grew}
-
-
 def metric_plot_logic():
     base_dict = get_random_dict()
     answer = [
         get_plot_string(base_dict["metric"], base_dict["date_min"], base_dict["date_max"], base_dict["plot_type"])
     ]
     return {**base_dict, "answer": answer}
-
 
 def distribution_plot_on_day_logic():
     base_dict = get_random_dict()
@@ -122,23 +89,67 @@ def distribution_plot_on_day_two_metrics_logic():
     return {**base_dict, "answer": answer}
 
 
-def metric_more_or_less_plot_logic():
-    query_info = get_threshold_and_metric_more_or_less()
+def metric_more_or_less_any_time(metric, date_min, threshold):
+    series_func = metric_to_func_dict[metric]
+    metric_series = pd.Series(series_func(date_min))
+    if (metric_series > threshold).sum() == 0:
+        return "less"
+    elif (metric_series < threshold).sum() == 0:
+        return "more"
+    else:
+        return "both more and less"
+
+def get_threshold_and_metric_more_or_less(date_min=None):
+    base_dict = get_random_dict()
+    date_min = base_dict["date_min"] if date_min is None else date_min
+    metric_vs_threshold = metric_more_or_less_any_time(base_dict["metric"], date_min, base_dict["threshold"])
+    return {**base_dict, "metric_vs_threshold": metric_vs_threshold}
+
+def metric_more_or_less_plot_logic(date_min=None):
+    query_info = get_threshold_and_metric_more_or_less(date_min)
+    query_info["date_min"] = date_min if date_min is not None else query_info["date_min"]
     if query_info["more_or_less"] in query_info["metric_vs_threshold"]:
         answer = [get_plot_string(query_info["metric"], query_info["date_min"], query_info["date_max"], "line")]
     else:
         answer = []
     return {"answer": answer, **query_info}
 
+def metric_more_or_less_past_weeks_plot_logic():
+    n_weeks = random.choice([1, 2, 3, 4, 5, 6])
+    date_min = str(HARDCODED_CURRENT_TIME.date() - pd.Timedelta(n_weeks, "W"))
+    return {**metric_more_or_less_plot_logic(date_min), "past_n_weeks": n_weeks}
 
-def metric_fell_or_grew_plot_logic():
-    query_info = get_metric_fell_vs_grew()
-    if query_info["fell_or_grew"] == query_info["fell_vs_grew"]:
+def metric_fallen_or_grown(metric, date_min, threshold=0):
+    metric_series = pd.Series(metric_to_func_dict[metric](date_min))
+    previous_value = metric_series[date_min]
+    current_value = metric_series.iloc[-1]
+    pct_change = (current_value - previous_value) / previous_value
+    if pct_change < 0 and abs(pct_change) > threshold:
+        return "fallen"
+    elif pct_change > 0 and pct_change > threshold:
+        return "grown"
+    else:
+        return "not changed"
+
+def get_threshold_and_fallen_or_grown(date_min=None):
+    base_dict = get_random_dict()
+    date_min = base_dict["date_min"] if date_min is None else date_min
+    fallen_or_grown = metric_fallen_or_grown(base_dict["metric"], date_min, base_dict["growth_threshold"])
+    return {**base_dict, "growth_vs_threshold": fallen_or_grown}
+
+def metric_fallen_or_grown_plot_logic(date_min=None):
+    query_info = get_threshold_and_fallen_or_grown(date_min)
+    query_info["date_min"] = date_min if date_min is not None else query_info["date_min"]
+    if query_info["fallen_or_grown"] == query_info["growth_vs_threshold"]:
         answer = [get_plot_string(query_info["metric"], query_info["date_min"], query_info["date_max"], "line")]
     else:
         answer = []
     return {"answer": answer, **query_info}
 
+def metric_fallen_or_grown_past_weeks_plot_logic():
+    n_weeks = random.choice([1, 2, 3, 4, 5, 6])
+    date_min = str(HARDCODED_CURRENT_TIME.date() - pd.Timedelta(n_weeks, "W"))
+    return {**metric_fallen_or_grown_plot_logic(date_min), "past_n_weeks": n_weeks}
 
 def metric_two_plots_logic():
     base_dict = get_random_dict()
@@ -155,29 +166,38 @@ ANALYTICS_TEMPLATES = [
         "logic": metric_plot_logic,
     },
     {
-        "query": """Plot the distribution of {natural_language_metric} on {natural_language_date}""",
-        "logic": distribution_plot_on_day_logic,
-    },
-    {
-        "query": """Can you chart the distribution of both {natural_language_metric} and {natural_language_metric_2} between {date_min} and {date_max}?""",
+        "query": """Can you plot the distribution of both {natural_language_metric} and {natural_language_metric_2} between {date_min} and {date_max}?""",
         "logic": distribution_plot_on_day_two_metrics_logic,
     },
-    {
-        "query": """If {natural_language_metric} has been {more_or_less} than {threshold} at any time since {natural_language_date}, make a line plot of it since then""",
-        "logic": metric_more_or_less_plot_logic,
-    },
+    
     {
         "query": """Make bar charts showing {natural_language_metric} and {natural_language_metric_2} since {date_min}""",
         "logic": metric_two_plots_logic,
+    },
+    {
+        "query": """If {natural_language_metric} was {more_or_less} than {threshold} at any time since {natural_language_date}, make a line plot of it since then""",
+        "logic": metric_more_or_less_plot_logic,
+    },
+    {
+        "query": """If {natural_language_metric} was {more_or_less} than {threshold} any time in the last {past_n_weeks} weeks, make a line plot of it since then""",
+        "logic": metric_more_or_less_past_weeks_plot_logic,
+    },
+    {
+        "query": """If {natural_language_metric} has {fallen_or_grown} by more than {natural_language_growth_threshold} since {natural_language_date}, make a line plot of it since then""",
+        "logic": metric_fallen_or_grown_plot_logic,
+    },
+    {
+        "query": """If {natural_language_metric} has {fallen_or_grown} by more than {natural_language_growth_threshold} in the last {past_n_weeks} weeks, make a line plot over that period""",
+        "logic": metric_fallen_or_grown_past_weeks_plot_logic,
     },
 ]
 for d in ANALYTICS_TEMPLATES:
     d["domains"] = ["analytics"] 
 
-max_queries_per_template = 10  # Limit the number of queries per template
+max_queries_per_template = 3  # Limit the number of queries per template
 
 if __name__ == "__main__":
-    generated_queries_and_answers = generate_all_queries_and_answers(ANALYTICS_TEMPLATES[-2:-1], max_queries_per_template)
+    generated_queries_and_answers = generate_all_queries_and_answers(ANALYTICS_TEMPLATES[3:], max_queries_per_template)
     df = pd.DataFrame(generated_queries_and_answers)
     df.to_csv(
         "data/processed/queries_and_answers/analytics_queries_and_answers.csv",
--- src/tools/analytics.py
+++ src/tools/analytics.py
@@ -3,8 +3,8 @@ from langchain.tools import tool
 
 ANALYTICS_DATA = pd.read_csv("data/processed/analytics_data.csv", dtype=str)
 PLOTS_DATA = pd.DataFrame(columns=["file_path"])
-METRICS = ["page_views", "session_duration_seconds"]
-METRIC_NAMES = ["total page views", "average session duration"]
+METRICS = ["total_visits", "session_duration_seconds"]
+METRIC_NAMES = ["total visits", "average session duration"]
 
 
 def reset_state():
@@ -59,7 +59,7 @@ def create_plot(time_min=None, time_max=None, value_to_plot=None, plot_type=None
     time_max : str, optional
         End date of the time range. Date format is "YYYY-MM-DD".
     value_to_plot : str, optional
-        Value to plot. Available values are: "page_views", "session_duration_seconds", "traffic_source", "user_engaged"
+        Value to plot. Available values are: "total_visits", "session_duration_seconds", "traffic_source", "user_engaged"
     plot_type : str, optional
         Type of plot. Can be "bar", "line", "scatter" or "histogram"
 
@@ -70,8 +70,8 @@ def create_plot(time_min=None, time_max=None, value_to_plot=None, plot_type=None
 
     Examples
     --------
-    >>> analytics.create_plot("2023-10-01", "2023-12-31", "page_views")
-    "plots/2023-10-01_2023-12-31_page_views.png"
+    >>> analytics.create_plot("2023-10-01", "2023-12-31", "total_visits")
+    "plots/2023-10-01_2023-12-31_total_visits.png"
 
     """
     global PLOTS_DATA
@@ -80,7 +80,7 @@ def create_plot(time_min=None, time_max=None, value_to_plot=None, plot_type=None
     if not time_max:
         return "End date not provided."
     if value_to_plot not in METRICS:
-        return "Value to plot must be one of 'page_views', 'session_duration_seconds', 'traffic_source', 'user_engaged'"
+        return "Value to plot must be one of 'total_visits', 'session_duration_seconds', 'traffic_source', 'user_engaged'"
     if plot_type not in ["bar", "line", "scatter", "histogram"]:
         return "Plot type must be one of 'bar', 'line', 'scatter', or 'histogram'"
 
@@ -110,7 +110,7 @@ def total_visits_count(time_min=None, time_max=None):
     Examples
     --------
     >>> analytics.total_visits_count("2023-10-01", "2023-10-06")
-    {"2023-10-01": 1, "2023-10-02": 2, "2023-10-03": 3, "2023-10-04": 1, "2023-10-05": 0, "2023-10-06": 4}
+    {{"2023-10-01": 1, "2023-10-02": 2, "2023-10-03": 3, "2023-10-04": 1, "2023-10-05": 0, "2023-10-06": 4}}
     """
     if time_min:
         data = ANALYTICS_DATA[ANALYTICS_DATA["date_of_visit"] >= time_min]
@@ -141,7 +141,7 @@ def engaged_users_count(time_min=None, time_max=None):
     Examples
     --------
     >>> analytics.engaged_users_count("2023-10-01", "2023-10-06")
-    {"2023-10-01": 1, "2023-10-02": 2, "2023-10-03": 2, "2023-10-04": 1, "2023-10-05": 0, "2023-10-06": 4}
+    {{"2023-10-01": 1, "2023-10-02": 2, "2023-10-03": 2, "2023-10-04": 1, "2023-10-05": 0, "2023-10-06": 4}}
     """
     if time_min:
         data = ANALYTICS_DATA[ANALYTICS_DATA["date_of_visit"] >= time_min]
@@ -175,7 +175,7 @@ def traffic_source_count(time_min=None, time_max=None, traffic_source=None):
     Examples
     --------
     >>> analytics.traffic_source_count("2023-10-01", "2023-10-06", "search engine")
-    {"2023-10-01": 0, "2023-10-02": 1, "2023-10-03": 0, "2023-10-04": 3, "2023-10-05": 2, "2023-10-06": 4}
+    {{"2023-10-01": 0, "2023-10-02": 1, "2023-10-03": 0, "2023-10-04": 3, "2023-10-05": 2, "2023-10-06": 4}}
     """
     if time_min:
         data = ANALYTICS_DATA[ANALYTICS_DATA["date_of_visit"] >= time_min]
@@ -191,7 +191,7 @@ def traffic_source_count(time_min=None, time_max=None, traffic_source=None):
         return data.groupby("date_of_visit").size().to_dict()
 
 
-@tool("analytics.get_average_session_duration", return_direct=False)
+@tool("analytics.get_average_session_duration", return_direct=True)
 def get_average_session_duration(time_min=None, time_max=None):
     """
     Returns the average session duration within a specified time range.
@@ -211,7 +211,7 @@ def get_average_session_duration(time_min=None, time_max=None):
     Examples
     --------
     >>> analytics.get_average_session_duration("2023-10-01", "2023-10-06")
-    {"2023-10-01": 10.0, "2023-10-02": 20.5, "2023-10-03": 32.8, "2023-10-04": 40.2, "2023-10-05": 5.3, "2023-10-06": 53.0}
+    {{"2023-10-01": 10.0, "2023-10-02": 20.5, "2023-10-03": 32.8, "2023-10-04": 40.2, "2023-10-05": 5.3, "2023-10-06": 53.0}}
     """
     if time_min:
         data = ANALYTICS_DATA[ANALYTICS_DATA["date_of_visit"] >= time_min]
